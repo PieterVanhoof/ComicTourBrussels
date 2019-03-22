f50aa6c5340dbc0ddb98711e52ba117d8cf44b2d
Jorn Rymenans <jorn.rymenans@gmail.com>
2019-03-22 13:55:05 +0100

    Images saved on data => data => ehb.be.comictourbrussels => files => myAppData => images

--- app/src/main/java/ehb/be/comictourbrussels/Utils/ComicHandler.java
+++ app/src/main/java/ehb/be/comictourbrussels/Utils/ComicHandler.java
@@ -1,13 +1,24 @@
 package ehb.be.comictourbrussels.Utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import ehb.be.comictourbrussels.Room.Comic;
 import ehb.be.comictourbrussels.Room.ComicDatabase;
@@ -53,8 +64,6 @@ public class ComicHandler extends Handler {
 
                 double lat = (double) coordinate.get(0);
                 double lng = (double) coordinate.get(1);
-                Log.d("TEST LAT", lat+"");
-                Log.d("TEST lng", lng+"");
 
 
 
@@ -62,22 +71,51 @@ public class ComicHandler extends Handler {
                 String imgID = (imageArray.getString("id") != null)? imageArray.getString("id"): "Unknown";
 
 
+                // Define where you want to save the file
+                String dirName = context.getFilesDir().getAbsolutePath() + "/myAppData/images";
+                final File storageDir = new File(dirName);
+                // Create directory
+                if (!storageDir.exists()) {
+                    storageDir.mkdirs();
+                }
+                // Your image address. ex: "http://http://stackoverflow.com/myImages.jpg"
+                String MY_IMAGE_URL = "https://opendata.brussel.be/explore/dataset/comic-book-route/files/"+imgID+"/300/";
+                final ImageView ivComic = new ImageView(context);
+                final String img_path = storageDir.getAbsolutePath() + "/" +imgID+ "ComicRoute.jpg";
+                Picasso.get().load(MY_IMAGE_URL).into(ivComic, new com.squareup.picasso.Callback() {
+                    @Override
+                    public void onSuccess() {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Save bitmap to local
+                                Bitmap bitmap = ((BitmapDrawable)ivComic.getDrawable()).getBitmap();
+                                File file = new File(img_path);
+                                try {
+                                    file.createNewFile();
+                                    FileOutputStream ostream = new FileOutputStream(file);
+                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, ostream);
+                                    ostream.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },100);
+                    }
+                    @Override
+                    public void onError(Exception e) {
+
+                    }
+                });
 
                 Comic currentComic = new Comic(lat,lng,personage,author,imgID);
                 ComicDatabase.getInstance(context).getComicDAO().insertComic(currentComic);
 
-
-
                 index++;
 
                 //Log.d("TEST personage", personage);
-                Log.d("TEST image" , filename);
 
             }
-
-
-
-
 
 
         } catch (JSONException e) {
