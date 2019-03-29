c49357ef6e81fa710e65885e81431d6d0d272e6e
Jorn Rymenans <jorn.rymenans@gmail.com>
2019-03-29 11:16:49 +0100

    filter restaurant

--- app/src/main/java/ehb/be/comictourbrussels/Room/RestaurantDAO.java
+++ app/src/main/java/ehb/be/comictourbrussels/Room/RestaurantDAO.java
@@ -22,6 +22,8 @@ public class RestaurantDAO {
 
         Restaurants.add(new Restaurant(new LatLng(50.845171, 4.346929), "Moeder Lambic fontainas","Tof Restaurant!"));
         Restaurants.add(new Restaurant(new LatLng(50.847610, 4.350156), "Balls & Glory!","Tof Restaurant!"));
+        Restaurants.add(new Restaurant(new LatLng(50.841041, 4.338798), "BarBQ café", "Mmmmmh Bacon!"));
+        Restaurants.add(new Restaurant(new LatLng(50.842434, 4.345808), "Comme Chez Soi", "Tof Restaurant!"));
 
 
         return Restaurants;
